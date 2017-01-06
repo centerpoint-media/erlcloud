b247156cd80c4e5013136a5c3a1aa22e691444d5
Derek Brown <derekbrown121@gmail.com>
2017-01-06 17:37:51 -0500

    Adjust the ML call

--- src/erlcloud_ml.erl
+++ src/erlcloud_ml.erl
@@ -11,27 +11,34 @@
 -include("erlcloud_aws.hrl").
 
 predict() ->
-  timer:tc(?MODULE, predict,
-  %predict([
-  [[
-            {<<"bid">>, <<"5">>}, 
-            {<<"domain">>, <<"cnn.com">>}, 
-            {<<"lid">>, <<"1321789">>}, 
-            {<<"floor">>, <<"5">>}, 
-            {<<"ip">>, <<"53.32.122.32">>}, 
-            {<<"price">>, <<"5">>}]]).
+  timer:tc(?MODULE, predict, [[
+    {<<"Var01">>, <<"123">>}, % {<<"crid">>, <<"123">>},
+    {<<"Var02">>, <<"cnn.com">>}, % {<<"domain">>, <<"cnn.com">>},
+    {<<"Var03">>, <<"1321789">>}, % {<<"lid">>, <<"1321789">>},
+    {<<"Var04">>, <<"large">>}, % {<<"playersize">>, <<"large">>},
+    {<<"Var05">>, <<"53.32.122.32">>}, % {<<"ip">>, <<"53.32.122.32">>},
+    {<<"Var06">>, <<"a">>}, % {<<"device">>, <<"a">>},
+    {<<"Var07">>, <<"b">>}, % {<<"os">>, <<"b">>},
+    {<<"Var08">>, <<"en">>}, % {<<"lang">>, <<"en">>},
+    {<<"Var09">>, <<"234">>}, % {<<"sid">>, <<"234">>},
+    {<<"Var10">>, <<"345">>}, % {<<"pubid">>, <<"345">>},
+    {<<"Var11">>, <<"false">>}, % {<<"userid">>, <<"456">>},
+    {<<"Var12">>, <<"567">>}, % {<<"aid">>, <<"567">>},
+    {<<"Var13">>, <<"5">>} % {<<"floor">>, <<"5">>}
+  ]]).
 
 -spec predict([{binary(), binary()}]) -> non_neg_integer().
 predict(Params) ->
-  Json = 
-  %{"MLModelId": ml-L97W74WlF66,
-   %"Record": {"bid": "5", "domain": "cnn.com", "lid": "1321789", "floor": "5", "ip": "53.32.122.32", "price": "5"
+  Json =
+    %{"MLModelId": ml-L97W74WlF66,
+  %"Record": {"bid": "5", "domain": "cnn.com", "lid": "1321789", "floor": "5", "ip": "53.32.122.32", "price": "5"
 
-  Json = [{<<"MLModelId">>, <<"ml-L97W74WlF66">>}, 
-          {<<"PredictEndpoint">>, <<"https://realtime.machinelearning.us-east-1.amazonaws.com">>},
-          %{<<"Record">>, [{<<"bid">>, <<"5">>}]}],
-          {<<"Record">>, Params}],
-  
+  %Json = [{<<"MLModelId">>, <<"ml-L97W74WlF66">>},
+  Json = [{<<"MLModelId">>, <<"ml-lyQ4THupSXn">>},
+    {<<"PredictEndpoint">>, <<"https://realtime.machinelearning.us-east-1.amazonaws.com">>},
+    %{<<"Record">>, [{<<"bid">>, <<"5">>}]}],
+    {<<"Record">>, Params}],
+
   % E.g.
   % {ok,[{<<"Prediction">>,
   %       [{<<"details">>,
@@ -42,11 +49,10 @@ predict(Params) ->
 
   case erlcloud_ml_impl:request(default_config(), "AmazonML_20141212.Predict", Json) of
     {ok, [{<<"Prediction">>, Result}]} ->
-      Label = proplists:get_value(<<"predictedLabel">>, Result),
-      {predicted_label, Label};
+      Value = proplists:get_value(<<"predictedValue">>, Result),
+      {predicted_value, Value};
     Err ->
       Err
   end.
 
 default_config() -> erlcloud_aws:default_config().
-
